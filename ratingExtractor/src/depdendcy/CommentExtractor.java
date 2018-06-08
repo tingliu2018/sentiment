9ee5a7af4194403801b7fc161d0632d447c5f9e4
LoganTheBrandt <35504647+LoganTheBrandt@users.noreply.github.com>
2018-06-08 15:22:02 -0400

    Checked for more different phrases

--- ratingExtractor/src/depdendcy/CommentExtractor.java
+++ ratingExtractor/src/depdendcy/CommentExtractor.java
@@ -19,21 +19,62 @@ public class CommentExtractor {
     static int goodCounter = 0;
     static int badCounter = 0;
     static int awfulCounter = 0;
+    static int horribleProfCounter = 0;
     static int horribleCounter = 0;
+    static int unfairCounter = 0;
+    static int fairCounter = 0;
+    static int loveCounter = 0;
+    static int bestCounter = 0;
+    static int worstCounter = 0;
+    static int interestingCounter = 0;
+    static int boringCounter = 0;
+    static int hardCounter = 0;
+    static int easyCounter = 0;
 
     public static void extractWords(String sentence) {
-        if (sentence.contains("awesome professor")) {
+        if (sentence.contains("awesome professor") || sentence.contains("awesome teacher")) {
             awesomeCounter++;
-        } else if (sentence.contains("wonderful professor")) {
+        } else if (sentence.contains("wonderful professor") || sentence.contains("wonderful teacher")) {
             wonderfulCounter++;
-        } else if (sentence.contains("good professor")) {
+        } else if (sentence.contains("good professor")  || sentence.contains("good teacher")) {
             goodCounter++;
-        } else if (sentence.contains("bad professor")) {
+        } else if (sentence.contains("bad professor")  || sentence.contains("bad teacher")) {
             badCounter++;
-        } else if (sentence.contains("awful professor")) {
+        } else if (sentence.contains("awful professor")  || sentence.contains("awful teacher")) {
             awfulCounter++;
-        } else if (sentence.contains("horrible professor")) {
+        } else if (sentence.contains("horrible professor")  || sentence.contains("horrible teacher")) {
+            horribleProfCounter++;
             horribleCounter++;
+        }
+        else if (sentence.contains("horrible")) {
+            horribleCounter++;
+        }
+        else if (sentence.contains("unfair")) {
+            unfairCounter++;
+        }
+        else if (sentence.contains("fair")) {
+            fairCounter++;
+        }
+        else if (sentence.contains("love")) {
+            loveCounter++;
+        }
+        else if (sentence.contains("best")) {
+            bestCounter++;
+        }
+        else if (sentence.contains("worst")) {
+            worstCounter++;
+        }
+        else if (sentence.contains("interesting")) {
+            interestingCounter++;
+        }
+        else if (sentence.contains("boring") || sentence.contains("bored")) {
+            boringCounter++;
+        }
+        else if (sentence.contains("hard")) {
+            hardCounter++;
+        }
+        else if (sentence.contains("easy")) {
+            easyCounter++;
         }
     }
 
@@ -47,14 +88,25 @@ public class CommentExtractor {
             str = sc.nextLine();
             extractWords(str);
         }
-        pw.println("awesome: " + awesomeCounter);
-        pw.println("good: " + goodCounter);
-        pw.println("wonderful: " + wonderfulCounter);
-        pw.println("bad: " + badCounter);
-        pw.println("awful: " + awfulCounter);
+        pw.println("awesome prof: " + awesomeCounter);
+        pw.println("good prof: " + goodCounter);
+        pw.println("wonderful prof: " + wonderfulCounter);
+        pw.println("bad prof: " + badCounter);
+        pw.println("awful prof: " + awfulCounter);
+        pw.println("horrible prof: " + horribleProfCounter);
         pw.println("horrible: " + horribleCounter);
+        pw.println("unfair: " + unfairCounter);
+        pw.println("fair: " + fairCounter);
+        pw.println("love: " + loveCounter);
+        pw.println("best: " + bestCounter);
+        pw.println("worst: " + worstCounter);
+        pw.println("interesting: " + interestingCounter);
+        pw.println("boring: " + boringCounter);
+        pw.println("hard: " + interestingCounter);
+        pw.println("easy: " + boringCounter);
         pw.flush();
         pw.close();
         sc.close();
     }
 }
+
