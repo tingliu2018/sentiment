5540b5aa9655b000aac730ad1b938c0ed1836c93
Thomas Talasco <34171760+talathom@users.noreply.github.com>
2019-06-04 12:16:50 -0400

    Update CombineFiles.java
    Rewrote File

--- CombineFiles.java
+++ CombineFiles.java
@@ -13,130 +13,120 @@ import java.util.Scanner;
 
 /**
  *
- * @author thomas
+ * @author Yuwei Chen
  */
 public class CombineFiles {
 
     public static void main(String[] args) throws FileNotFoundException {
-        File[] knownFiles = {new File("/media/thomas/ESD-USB/Need Score/FemaleAverageKnownOutput.txt"),
+
+        File[] femaleKnownFiles = {
+            new File("/media/thomas/ESD-USB/Need Score/FemaleAverageKnownOutput.txt"),
             new File("/media/thomas/ESD-USB/Need Score/FemaleAwesomeKnownOutput.txt"),
             new File("/media/thomas/ESD-USB/Need Score/FemaleGoodKnownOutput.txt"),
             new File("/media/thomas/ESD-USB/Need Score/FemalePoorKnownOutput.txt"),
+            new File("/media/thomas/ESD-USB/Need Score/FemaleAwfulKnownOutput.txt"),};
+
+        File[] maleKnownFiles = {
             new File("/media/thomas/ESD-USB/Need Score/MaleAverageKnownOutput.txt"),
             new File("/media/thomas/ESD-USB/Need Score/MaleAwesomeKnownOutput.txt"),
             new File("/media/thomas/ESD-USB/Need Score/MaleGoodKnownOutput.txt"),
-            new File("/media/thomas/ESD-USB/Need Score/MalePoorKnownOutput.txt")};
+            new File("/media/thomas/ESD-USB/Need Score/MalePoorKnownOutput.txt"),
+            new File("/media/thomas/ESD-USB/Need Score/MaleAwfulKnownOutput.txt")};
 
-        File[] unknownFiles = {new File("/media/thomas/ESD-USB/Need Score/FemaleAverageUnknownOutput.txt"),
+        File[] femaleUnknownFiles = {
+            new File("/media/thomas/ESD-USB/Need Score/FemaleAverageUnknownOutput.txt"),
             new File("/media/thomas/ESD-USB/Need Score/FemaleAwesomeUnknownOutput.txt"),
             new File("/media/thomas/ESD-USB/Need Score/FemaleGoodUnknownOutput.txt"),
             new File("/media/thomas/ESD-USB/Need Score/FemalePoorUnknownOutput.txt"),
+            new File("/media/thomas/ESD-USB/Need Score/FemaleAwfulUnknownOutput.txt"),};
+
+        File[] maleUnknownFiles = {
             new File("/media/thomas/ESD-USB/Need Score/MaleAverageUnknownOutput.txt"),
             new File("/media/thomas/ESD-USB/Need Score/MaleAwesomeUnknownOutput.txt"),
             new File("/media/thomas/ESD-USB/Need Score/MaleGoodUnknownOutput.txt"),
-            new File("/media/thomas/ESD-USB/Need Score/MalePoorUnknownOutput.txt")};
+            new File("/media/thomas/ESD-USB/Need Score/MalePoorUnknownOutput.txt"),
+            new File("/media/thomas/ESD-USB/Need Score/MaleAwfulUnknownOutput.txt")};
 
-        Scanner fileRead;
-        ArrayList<String> words = new ArrayList<String>();
-        ArrayList<Integer> wordCounts = new ArrayList<Integer>();
+        ArrayList<Word> maleWords = new ArrayList<Word>();
+        ArrayList<Word> femaleWords = new ArrayList<Word>();
+        ArrayList<Word> combinedWords = new ArrayList<Word>();
 
-        for (int i = 0; i < knownFiles.length / 2; i++) {
-            fileRead = new Scanner(knownFiles[i]);
-            parseLine(fileRead, words, wordCounts);
-            fileRead = new Scanner(knownFiles[i + 4]);
-            int index = 0;
-            while (fileRead.hasNextLine()) {
-                String line = fileRead.nextLine();
-                String[] splitLine = line.split(" ");
-                wordCounts.set(index, wordCounts.get(index) + Integer.parseInt(splitLine[splitLine.length - 1]));
-                index++;
-            }
-            printFile(words, wordCounts, i, true);
+        String[] outputKnownStrings = {"KnownAverage","KnownAwesome","KnownGood","KnownPoor","KnownAwful",};
+        String[] outputUnknownStrings = {"UnknownAverage","UnknownAwesome","UnknownGood","UnknownPoor","UnknownAwful"};
+        
+        for (int i = 0; i < maleKnownFiles.length; i++) {
+            femaleWords = parseFile(femaleKnownFiles[i]);
+            maleWords = parseFile(maleKnownFiles[i]);
+            combinedWords = combineFiles(femaleWords, maleWords);
+            printFile(combinedWords,outputKnownStrings[i]);
+            femaleWords.clear();
+            maleWords.clear();
+            combinedWords.clear();
         }
-
-        for (int i = 0; i < unknownFiles.length / 2; i++) {
-            fileRead = new Scanner(unknownFiles[i]);
-            parseLine(fileRead, words, wordCounts);
-            fileRead = new Scanner(knownFiles[i + 4]);
-            while (fileRead.hasNextLine()) {
-                String line = fileRead.nextLine();
-                String[] splitLine = line.split(" ");
-                int index = -1;
-                for (int j = 0; j < words.size(); j++) {
-                    if (words.get(j).equals(splitLine[0])) {
-                        index = j;
-                        break;
-                    }
-                }
-                if (index == -1) {
-                    if (splitLine.length > 2) {
-                        String word = "";
-                        for (int j = 0; j < splitLine.length - 1; j++) {
-                            word += splitLine[j];
-                        }
-                        words.add(word);
-                        wordCounts.add(Integer.parseInt(splitLine[splitLine.length - 1]));
-                    } else {
-                        words.add(splitLine[0]);
-                        wordCounts.add(Integer.parseInt(splitLine[splitLine.length - 1]));
-                    }
-                } else {
-                    wordCounts.set(index, wordCounts.get(index) + Integer.parseInt(splitLine[splitLine.length - 1]));
-                }
-            }
-            printFile(words, wordCounts, i, false);
-
+        for (int i = 0; i < maleUnknownFiles.length; i++) {
+            femaleWords = parseFile(femaleUnknownFiles[i]);
+            maleWords = parseFile(femaleUnknownFiles[i]);
+            combinedWords = combineFiles(femaleWords, maleWords);
+            printFile(combinedWords,outputUnknownStrings[i]);
+            femaleWords.clear();
+            maleWords.clear();
+            combinedWords.clear();
         }
+        
     }
 
-    public static void printFile(ArrayList<String> words, ArrayList<Integer> wordCounts, int i, boolean known) throws FileNotFoundException {
-        String fileName = "txt.txt";
-        if (known) {
-            if (i < 1) {
-                fileName = "/media/thomas/ESD-USB/Combined Files/KnownAverage.txt";
-            } else if (i < 2) {
-                fileName = "/media/thomas/ESD-USB/Combined Files/KnownAwesome.txt";
-            } else if (i < 3) {
-                fileName = "/media/thomas/ESD-USB/Combined Files/KnownGood.txt";
-            } else if (i < 4) {
-                fileName = "/media/thomas/ESD-USB/Combined Files/KnownPoor.txt";
+    public static ArrayList<Word> combineFiles(ArrayList<Word> femaleWords, ArrayList<Word> maleWords) {
+        ArrayList<Word> combinedWords = femaleWords;
+        boolean flag = false;
+        for (int i = 0; i < maleWords.size(); i++) {
+            for (int j = 0; j < combinedWords.size(); j++) {
+                if (combinedWords.get(j).getWord().equals(maleWords.get(i).getWord())) {
+                    combinedWords.get(j).setCount(combinedWords.get(j).getCount() + maleWords.get(i).getCount());
+                    flag = true;
+                    break;
+                }
             }
-        } else {
-            if (i < 1) {
-                fileName = "/media/thomas/ESD-USB/Combined Files/UnknownAverage.txt";
-            } else if (i < 2) {
-                fileName = "/media/thomas/ESD-USB/Combined Files/UnknownAwesome.txt";
-            } else if (i < 3) {
-                fileName = "/media/thomas/ESD-USB/Combined Files/UnknownGood.txt";
-            } else if (i < 4) {
-                fileName = "/media/thomas/ESD-USB/Combined Files/UnknownPoor.txt";
+            if (!flag) {
+                combinedWords.add(maleWords.get(i));
+            } else {
+                flag = false;
             }
         }
-        PrintWriter pw = new PrintWriter(fileName);
+        return combinedWords;
+    }
+
+    public static void printFile(ArrayList<Word> words, String fileName) throws FileNotFoundException {
+        String filePath = "/media/thomas/ESD-USB/Combined Files/" + fileName + ".txt";
+
+        PrintWriter pw = new PrintWriter(filePath);
+        //System.out.println(words.toString());
         for (int j = 0; j < words.size(); j++) {
-            pw.println(words.get(j) + " " + wordCounts.get(j));
+            pw.println(words.get(j).getWord() + " " + words.get(j).getCount());
         }
-        words.clear();
-        wordCounts.clear();
+        pw.close();
+        //System.out.println(words.toString());
     }
 
-    public static void parseLine(Scanner fileRead, ArrayList<String> words, ArrayList<Integer> wordCounts) {
+    public static ArrayList<Word> parseFile(File file) throws FileNotFoundException {
+        Scanner fileRead = new Scanner(file);
+        //read file
+
+        ArrayList<Word> words = new ArrayList<Word>();
         while (fileRead.hasNextLine()) {
+            //splits file line into word and it's count
             String line = fileRead.nextLine();
             String[] splitLine = line.split(" ");
             if (splitLine.length < 3) {
-                words.add(splitLine[0]);
-                wordCounts.add(Integer.parseInt(splitLine[1]));
+                words.add(new Word(splitLine[0], Integer.parseInt(splitLine[1])));
             } else {
                 String word = "";
                 for (int j = 0; j < splitLine.length - 1; j++) {
                     word += splitLine[j];
                 }
-                words.add(word);
-                //System.out.println(splitLine[0] + " " + splitLine[1]);
-                wordCounts.add(Integer.parseInt(splitLine[splitLine.length - 1]));
+                words.add(new Word(word, Integer.parseInt(splitLine[splitLine.length - 1])));
             }
         }
+        return words;
     }
 
 }
