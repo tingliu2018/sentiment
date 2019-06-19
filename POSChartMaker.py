8bd0d657f588ef3129b43601619eb210815b6dea
Thomas Talasco <34171760+talathom@users.noreply.github.com>
2019-06-19 12:55:20 -0400

    Add files via upload
    Makes Files with words sorted by POS

---
+++ POSChartMaker.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 19 12:10:38 2019
+
+@author: thomas
+"""
+
+import pandas as pd
+
+def linearSearch(search, arr):
+    for i in range(0, len(arr)):
+        if arr[i] == search:
+            return i
+    return -1
+
+pdData = pd.read_csv('betterbigbadfile.csv', header=0)
+words = pdData.Word
+ourscore = pdData.Our_Score
+lexscore = pdData.Lex_Score
+totalcount = pdData.Total_Count
+awesomecount = pdData.Awesome_Count
+goodcount = pdData.Good_Count
+averagecount = pdData.Average_Count
+poorcount = pdData.Poor_Count
+awfulcount = pdData.Awful_Count
+
+nounfile = open('/media/thomas/ESD-USB/Parts of Speech/Noun.txt', 'r')
+verbfile = open('/media/thomas/ESD-USB/Parts of Speech/Verb.txt', 'r')
+adjectivefile = open('/media/thomas/ESD-USB/Parts of Speech/Adjective.txt', 'r')
+adverbfile = open('/media/thomas/ESD-USB/Parts of Speech/Adverb.txt', 'r')
+nounchart = open('/media/thomas/ESD-USB/Parts of Speech/NounChart.csv', 'w')
+verbchart = open('/media/thomas/ESD-USB/Parts of Speech/VerbChart.csv', 'w')
+adjectivechart = open('/media/thomas/ESD-USB/Parts of Speech/AdjectiveChart.csv', 'w')
+adverbchart = open('/media/thomas/ESD-USB/Parts of Speech/AdverbChart.csv', 'w')
+
+header = "Word\tOur_Score\tLex_Score\tTotal_Count\tAwesome_Count\tGood_Count\tAverage_Count\tPoor_Count\tAwful_Count\n"
+nounchart.write(header)
+verbchart.write(header)
+adjectivechart.write(header)
+adverbchart.write(header)
+
+nouns = nounfile.readlines()
+verbs = verbfile.readlines()
+adjectives = adjectivefile.readlines()
+adverbs = adverbfile.readlines()
+
+for i in range(0, len(nouns)):
+    nouns[i] = nouns[i].rstrip()
+    
+for i in range(0, len(verbs)):
+    verbs[i] = verbs[i].rstrip()
+    
+for i in range(0, len(adjectives)):
+    adjectives[i] = adjectives[i].rstrip()
+    
+for i in range(0, len(adverbs)):
+    adverbs[i] = adverbs[i].rstrip()
+
+for i in range(0, len(words)):
+    index = linearSearch(words[i], nouns)
+    if index != -1:
+        nounchart.write(str(words[i]) +"\t"+ str(ourscore[i]) +"\t"+ str(lexscore[i]) +"\t"+ str(totalcount[i]) +"\t"+ str(awesomecount[i]) +"\t"+ str(goodcount[i]) +"\t"+ str(averagecount[i]) +"\t"+ str(poorcount[i]) +"\t"+ str(awfulcount[i]) +"\n")
+    index = linearSearch(words[i], verbs)
+    if index != -1:
+        verbchart.write(str(words[i]) +"\t"+ str(ourscore[i]) +"\t"+ str(lexscore[i]) +"\t"+ str(totalcount[i]) +"\t"+ str(awesomecount[i]) +"\t"+ str(goodcount[i]) +"\t"+ str(averagecount[i]) +"\t"+ str(poorcount[i]) +"\t"+ str(awfulcount[i]) +"\n")
+    index = linearSearch(words[i], adjectives)
+    if index != -1:
+        adjectivechart.write(str(words[i]) +"\t"+ str(ourscore[i]) +"\t"+ str(lexscore[i]) +"\t"+ str(totalcount[i]) +"\t"+ str(awesomecount[i]) +"\t"+ str(goodcount[i]) +"\t"+ str(averagecount[i]) +"\t"+ str(poorcount[i]) +"\t"+ str(awfulcount[i]) +"\n")
+    index = linearSearch(words[i], adverbs)
+    if index != -1:
+        adverbchart.write(str(words[i]) +"\t"+ str(ourscore[i]) +"\t"+ str(lexscore[i]) +"\t"+ str(totalcount[i]) +"\t"+ str(awesomecount[i]) +"\t"+ str(goodcount[i]) +"\t"+ str(averagecount[i]) +"\t"+ str(poorcount[i]) +"\t"+ str(awfulcount[i]) +"\n")
+    
+nounfile.close()
+verbfile.close()
+adjectivefile.close()
+adverbfile.close()
+
+nounchart.close()
+verbchart.close()
+adjectivechart.close()
+adverbchart.close()
---
+++ PartofSpeechFinder.java
@@ -0,0 +1,53 @@
+package parsertest;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import net.sf.extjwnl.JWNLException;
+import net.sf.extjwnl.data.IndexWord;
+import net.sf.extjwnl.data.POS;
+import net.sf.extjwnl.dictionary.Dictionary;
+
+/**
+ *
+ * @author Thomas
+ */
+public class PartofSpeechFinder {
+
+    public static void main(String[] args) throws JWNLException, FileNotFoundException {
+        File lexicon = new File("/media/thomas/ESD-USB/Scores.csv");
+        Dictionary dictionary = Dictionary.getDefaultResourceInstance();
+        Scanner sc = new Scanner(lexicon);
+
+        PrintWriter[] pw = new PrintWriter[4];
+        pw[0] = new PrintWriter("/media/thomas/ESD-USB/Parts of Speech/Noun.txt");
+        pw[1] = new PrintWriter("/media/thomas/ESD-USB/Parts of Speech/Verb.txt");
+        pw[2] = new PrintWriter("/media/thomas/ESD-USB/Parts of Speech/Adjective.txt");
+        pw[3] = new PrintWriter("/media/thomas/ESD-USB/Parts of Speech/Adverb.txt");
+        IndexWord[] iWords = new IndexWord[4];
+        
+        sc.nextLine();
+
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String word = line.substring(0, line.indexOf(','));
+            iWords[0] = dictionary.getIndexWord(POS.NOUN, word);
+            iWords[1] = dictionary.getIndexWord(POS.VERB, word);
+            iWords[2] = dictionary.getIndexWord(POS.ADJECTIVE, word);
+            iWords[3] = dictionary.getIndexWord(POS.ADVERB, word);
+
+            for (int i = 0; i < iWords.length; i++) {
+                if (iWords[i] != null) {
+                    pw[i].println(iWords[i].getLemma());
+                }
+
+            }
+        }
+        
+        for (int i = 0; i < pw.length; i++) {
+            pw[i].close();
+        }
+
+    }
+}
